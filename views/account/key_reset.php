74856b680ea3c929a0ad1ce966daf7f307dbf6ca
NaysKutzu <ghermancassian2008@gmail.com>
2023-12-06 17:37:30 +0000

    PUSH -> Added an option to reset the account key!

--- views/account/key_reset.php
+++ views/account/key_reset.php
@@ -1,6 +1,12 @@
 <?php 
-
-
-
-
+require(__DIR__ . '/../requirements/page.php');
+// Reset the key!
+$bIsSession = $session->resetKey($_COOKIE['token']);
+if ($bIsSession == true) {
+    header('location: /account');
+    die();
+} else {
+    header('location: /account?e=db_error');
+    die();
+}
 ?>
