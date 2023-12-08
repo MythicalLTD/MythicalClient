3cf51ae0d66806f0dfa4968b3d489abb87ea640f
NaysKutzu <ghermancassian2008@gmail.com>
2023-12-08 12:14:27 +0000

    PUSH -> Testing codeowner

---
+++ app/Google/2fa.php
@@ -0,0 +1,16 @@
+<?php 
+namespace MythicalClient\Google;
+
+class TwoFactorAuthentication {
+    /**
+     * NOTE: 
+     * 
+     * This code was not written by NaysKutzu or other people from MythicalSystems!
+     * The original author of this code is: Gilberto Cortez
+     * Copyright 2022 Gilberto Cortez
+     * Copyright 2023 MythicalSystems
+     * 
+     * 
+     */
+}
+?>
